96e76e4efaccf6e7e34df9120b8abab354f7e989
Rezwan <rezwanahmedtorabee@gmail.com>
2026-05-24 02:19:16 +0600

    My Project Upload

---
+++ app/src/main/java/com/application/App.java
@@ -0,0 +1,22 @@
+package com.application;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import java.util.Arrays;
+import java.util.List;
+
+public class App extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
